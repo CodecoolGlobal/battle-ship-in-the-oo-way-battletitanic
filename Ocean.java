d6fa3a0082d2b266b7af4c4eeb30681d4d8d5ffa
dziurawobrody <jaskijaski@gmail.com>
2020-05-24 13:28:19 +0200

    initiate ocean and ship classes

--- Ocean.java
+++ Ocean.java
@@ -1,3 +1,20 @@
+import java.util.List;
+
 public class Ocean {
     
-}
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
+    
+    private List<List<Square>> squares;
+    private List<Ship> ships;
+
+    public Ocean(List<List<Square>> squares,
+                 List<Ship> ships) {
+        this.squares = squares;
+        this.ships = ships;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
--- ship.java
+++ ship.java
@@ -1,3 +1,10 @@
+import java.util.List;
+
 public class Ship {
     
+    private List<Square> squares;
+
+    public Ship(List<Square> squares) {
+        this.squares = squares;
+    }
 }
