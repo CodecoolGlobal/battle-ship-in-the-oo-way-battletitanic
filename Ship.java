954238d15f9abdc38905bdaedd46ba7e7ea4184d
Bartosz Jakimko <barjakimko@gmail.com>
2020-05-24 13:31:34 +0200

    test commit

--- Ship.java
+++ Ship.java
@@ -1,3 +1,22 @@
+import java.util.List;
+
 public class Ship {
+
     
+    private List<Square> squares;
+    private int x;
+    private int y; 
+
+    public Ship(List<Square> squares, int x, int y){
+        this.squares = squares;
+        this.x = x;
+        this.y = y;
+    }
+
+    
+    @Override
+    public String toString(){
+
+        return "";
+    }
 }
