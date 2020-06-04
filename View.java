c5a5b5702f88f5fb41de7eeff9c220ab4c832c0a
kubenzi <60842782+kubenzi@users.noreply.github.com>
2020-06-04 18:34:46 +0200

    Created class View
    Implemented func:
    - clearScreen()
    - printTitle()
    - printInfo()
    - inputFromuser()
    - intFromUser()
    - pause()

---
+++ View.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class View {
+    
+    public void printTitle(String title) {
+        System.out.println("");
+        System.out.println(" " + title.toUpperCase());
+        System.out.println("");
+    }
+
+    public void clearScreen() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    
+    }
+
+    public void printInfo(String info) {
+        System.out.println(" " + info);
+
+    }
+
+    public String inputFromUser(String text) {
+        try {
+            System.out.println(" " + text + ":");
+            System.out.println(" ");
+            Scanner scannerFromUser = new Scanner(System.in);
+            String input = scannerFromUser.nextLine();
+            return input;
+        } finally {
+            in.close();
+        }
+        
+    }
+    
+    public int intFromUser(int text) {
+        try {
+            System.out.println(" " + text + ":");
+            System.out.println(" ");
+            Scanner scannerFromUser = new Scanner(System.in);
+            int input = scannerFromUser.nextInt();
+            return input;
+        } finally {
+            in.close();
+        }
+    }
+    
+    public void pause() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    
+}
